1b93cbb2012d833e525e9013a93bbaef06cd6b39
RumanAkhtar <cseruman42@gmail.com>
2024-02-02 21:59:40 +0530

    first commit

---
+++ Components/Navbar.jsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex py-3 flex-wrap justify-around' >
+        <h1 className='text-lg font-semibold' >TODO-APP</h1>
+        <ul className='flex gap-[40px] text-m' >
+            <li>HOME</li>
+            <li>PRODUCT</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
